8a245dd2cd50066c036656879b60be36121457f7
software1398 <scorpio1398@hanmail.net>
2020-11-24 20:00:09 +0900

    ocr_mlp modify

--- ocr_mlp.py
+++ ocr_mlp.py
@@ -4,9 +4,12 @@ pipeline = keras_ocr.pipeline.Pipeline()
 
 
 def pred(url):
-    prediction_group = pipeline.recognize([url])
-    result = []
-    for i in prediction_group[0]:
-        result.append(i[0])
-    text = ', '.join(result)
+    try:
+        prediction_group = pipeline.recognize([url])
+        result = []
+        for i in prediction_group[0]:
+            result.append(i[0])
+        text = ', '.join(result)
+    except Exception as e:
+        text = e
     return text
